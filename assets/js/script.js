59a98f165e50ac6e98dc85cb63c680a05204c49d
Rebaz Pappiland <rebazp@gmail.com>
2024-04-15 18:45:33 +0000

    change computer to opponent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 /* Constants, DOM elements and buttons */
 
 const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
+const opponentText = document.querySelector("#opponentText");
 const resultText = document.querySelector("#resultText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
 const playerScoreText = document.querySelector("#playerScore");
-const computerScoreText = document.querySelector("#computerScore");
+const opponentScoreText = document.querySelector("#opponentScore");
 const drawScoreText = document.querySelector("#drawScore");
 let player;
-let computer;
+let opponent;
 let playerScore = 0;
-let computerScore = 0;
+let opponentScore = 0;
 let drawScore = 0;
 
 /* Main game area */
@@ -18,9 +18,9 @@ let drawScore = 0;
 choiceBtns.forEach(button => button.addEventListener("click", () => {
 
     player = button.textContent;
-    computerTurn();
+    opponentTurn();
     playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `Computer: ${computer}`;
+    opponentText.textContent = `Opponent: ${opponent}`;
     result = checkWinner();
     resultText.textContent = result;
     updateScore(result);
@@ -28,25 +28,25 @@ choiceBtns.forEach(button => button.addEventListener("click", () => {
 
 /* Main game function */
 
-function computerTurn() {
+function opponentTurn() {
 
     const randNum = Math.floor(Math.random() * 5) + 1;
 
     switch (randNum) {
         case 1:
-            computer = "ROCK";
+            opponent = "ROCK";
             break;
         case 2:
-            computer = "PAPER";
+            opponent = "PAPER";
             break;
         case 3:
-            computer = "SCISSOR";
+            opponent = "SCISSOR";
             break;
         case 4:
-            computer = "LIZARD";
+            opponent = "LIZARD";
             break;
         case 5:
-            computer = "SPOCK";
+            opponent = "SPOCK";
             break;
 
     }
@@ -55,22 +55,22 @@ function computerTurn() {
 /* Winner function */
 
 function checkWinner() {
-    if (player == computer) {
+    if (player == opponent) {
         return "DRAW";
     }
-    else if (computer == "ROCK") {
+    else if (opponent == "ROCK") {
         return (player == "SCISSOR" || player == "LIZARD") ? "YOU LOSE!" : "YOU WIN!";
     }
-    else if (computer == "PAPER") {
+    else if (opponent == "PAPER") {
         return (player == "ROCK" || player == "SPOCK") ? "YOU LOSE!" : "YOU WIN!";
     }
-    else if (computer == "SCISSOR") {
+    else if (opponent == "SCISSOR") {
         return (player == "PAPER" || player == "LIZARD") ? "YOU LOSE!" : "YOU WIN!";
     }
-    else if (computer == "LIZARD") {
+    else if (opponent == "LIZARD") {
         return (player == "SPOCK" || player == "PAPER") ? "YOU LOSE!" : "YOU WIN!";
     }
-    else if (computer == "SPOCK") {
+    else if (opponent == "SPOCK") {
         return (player == "SCISSOR" || player == "ROCK") ? "YOU LOSE!" : "YOU WIN!";
     }
 }
@@ -81,11 +81,11 @@ function updateScore(result) {
     if (result === "YOU WIN!") {
         playerScore++;
     } else if (result === "YOU LOSE!") {
-        computerScore++;
+        opponentScore++;
     } else if (result === "DRAW") {
         drawScore++;
     }
     playerScoreText.textContent = `Player: ${playerScore}`;
-    computerScoreText.textContent = `Computer: ${computerScore}`;
+    opponentScoreText.textContent = `Opponent: ${opponentScore}`;
     drawScoreText.textContent = `Draw: ${drawScore}`;
 }
