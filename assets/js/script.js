bb0aab09308c3a04cb858160315a10ed521045cd
Rebaz Pappiland <rebazp@gmail.com>
2024-04-15 19:36:01 +0000

    fixed comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-/* Constants, DOM elements and buttons */
-
-const userText = document.querySelector("#userText");
-const opponentText = document.querySelector("#opponentText");
-const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
-const userScoreText = document.querySelector("#userScore");
-const opponentScoreText = document.querySelector("#opponentScore");
-const drawScoreText = document.querySelector("#drawScore");
+/* Constants, selecting DOM elements and buttons */
+const userText = document.querySelector("#userText");   // Select DOM to display user selection
+const opponentText = document.querySelector("#opponentText");   // Select DOM to display opponent selection
+const resultText = document.querySelector("#resultText");   // Select DOM to display result
+const choiceBtns = document.querySelectorAll(".choiceBtn"); // Select DOM to representing available buttons
+const userScoreText = document.querySelector("#userScore"); // Select DOM to display user score
+const opponentScoreText = document.querySelector("#opponentScore"); // Select DOM to display opponent score
+const drawScoreText = document.querySelector("#drawScore"); // Select DOM to display draw score
+// Store user and opponent selections
 let user;
 let opponent;
+// Store user, opponent and draw scores
 let userScore = 0;
 let opponentScore = 0;
 let drawScore = 0;
 
 /* Main game area */
-
 function initializeSelection(event) {   // Activate game after user selection
     user = event.target.textContent;    // Extract user selection
     opponentTurn(); // Trigger opponent turn to make selection
@@ -27,37 +27,37 @@ function initializeSelection(event) {   // Activate game after user selection
  }
 
 /* Main game function */
-
 function opponentTurn() {
 
-    const randNum = Math.floor(Math.random() * 5) + 1;
+    const randNum = Math.floor(Math.random() * 5) + 1;  // Generate random number between 1-5
 
+    // Switch statement to assign opponent's selection based on the random number
     switch (randNum) {
         case 1:
-            opponent = "ROCK";
+            opponent = "ROCK";      // If random number is 1, opponent chooses ROCK
             break;
         case 2:
-            opponent = "PAPER";
+            opponent = "PAPER";     // If random number is 2, opponent chooses PAPER
             break;
         case 3:
-            opponent = "SCISSOR";
+            opponent = "SCISSOR";   // If random number is 3, opponent chooses SCISSOR
             break;
         case 4:
-            opponent = "LIZARD";
+            opponent = "LIZARD";    // If random number is 4, opponent chooses LIZARD
             break;
         case 5:
-            opponent = "SPOCK";
+            opponent = "SPOCK";     // If random number is 5, opponent chooses SPOCK
             break;
 
     }
 }
 
 /* Winner function */
-
-function checkWinner() {
-    if (user == opponent) {
+function checkWinner() {    // Check who's the winner
+    if (user == opponent) { // Check for draw
         return "DRAW";
     }
+    // Check for win/lose based on opponents choice
     else if (opponent == "ROCK") {
         return (user == "SCISSOR" || user == "LIZARD") ? "YOU LOSE!" : "YOU WIN!";
     }
@@ -76,22 +76,20 @@ function checkWinner() {
 }
 
 /* Update score function */
-
-function updateScore(result) {
-    if (result === "YOU WIN!") {
-        userScore++;
+function updateScore(result) {  // Update score based on result
+    if (result === "YOU WIN!") {    // Check result and update score
+        userScore++;    // Incriment users score if they won
     } else if (result === "YOU LOSE!") {
-        opponentScore++;
+        opponentScore++;    // Incriment opponent score if they won
     } else if (result === "DRAW") {
-        drawScore++;
+        drawScore++;    // Incriment draw score if they game ended in draw
     }
-    userScoreText.textContent = `User: ${userScore}`;
-    opponentScoreText.textContent = `Opponent: ${opponentScore}`;
-    drawScoreText.textContent = `Draw: ${drawScore}`;
+    userScoreText.textContent = `User: ${userScore}`;   // Update user score text
+    opponentScoreText.textContent = `Opponent: ${opponentScore}`;   // Update opponent score text
+    drawScoreText.textContent = `Draw: ${drawScore}`;   // Update draw score text
 }
 
 /* DOM */
-
 document.addEventListener("DOMContentLoaded", (event) => {  // Event listener triggers when DOM is loaded
     choiceBtns.forEach(button => button.addEventListener("click", initializeSelection));    // Event listener attached to choice buttons to initialize selection
   });
